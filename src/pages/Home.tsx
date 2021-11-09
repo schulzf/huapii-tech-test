1ad368248ff0d4b22f2ca386860f67254e689a97
Felipe SCHULZ <56260943+schulzf@users.noreply.github.com>
2021-11-09 15:51:12 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
-import { Container } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AddExperienceBtn } from '../components/AddExperienceBtn';
 import { ExperienceCard } from '../components/ExperienceCard';
+import { Wrapper } from '../components/Wrapper';
 import { selectUserEducation } from '../redux/slices/educationSlice';
 import { getEducationRecords } from '../redux/thunks/experiencesThunks';
 import { colors } from '../utils/constants';
@@ -23,20 +23,18 @@ export const Home: React.FC<HomeProps> = () => {
   }, []);
 
   return (
-    <div className={styles.wrapper}>
-      <Container maxWidth='md' className={styles.container}>
-        <div className={`${globalStyles.flex} ${globalStyles.mb3}`}>
-          <h1 className={styles.title}>Education</h1>
-          <AddExperienceBtn size={42} color={colors.primary} />
-        </div>
+    <Wrapper>
+      <div className={`${globalStyles.flex} ${globalStyles.mb3}`}>
+        <h1 className={styles.title}>Education</h1>
+        <AddExperienceBtn size={42} color={colors.primary} />
+      </div>
 
-        {userEducation.map((item: any) => (
-          <ExperienceCard data={item} key={item.key} />
-        ))}
+      {userEducation.map((item: any) => (
+        <ExperienceCard data={item} key={item.key} />
+      ))}
 
-        {/* <pre>{JSON.stringify(userEducation, null, 2)}</pre> */}
-      </Container>
-    </div>
+      {/* <pre>{JSON.stringify(userEducation, null, 2)}</pre> */}
+    </Wrapper>
   );
 };
 
